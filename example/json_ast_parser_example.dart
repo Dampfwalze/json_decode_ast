5895829e49038fd559763bfd20eb881858d03fc5
Dampfwalze <46897578+Dampfwalze@users.noreply.github.com>
2025-02-01 14:02:20 +0100

    Initial (Template: `package`)

---
+++ example/json_ast_parser_example.dart
@@ -0,0 +1,6 @@
+import 'package:json_ast_parser/json_ast_parser.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/json_ast_parser_test.dart
@@ -0,0 +1,16 @@
+import 'package:json_ast_parser/json_ast_parser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}

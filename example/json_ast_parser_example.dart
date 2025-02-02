4e46ecec8743224b2da1f965ffa09da301fb0c09
Dampfwalze <46897578+Dampfwalze@users.noreply.github.com>
2025-02-02 01:09:31 +0100

    Add example

--- example/json_ast_parser_example.dart
+++ example/json_ast_parser_example.dart
@@ -1,3 +1,22 @@
 import 'package:json_ast_parser/json_ast_parser.dart';
 
-void main() {}
+void main() {
+  final json = '''
+{
+  "name": "John Doe",
+  "age": 30,
+  "isStudent": false,
+  "scores": [100, 200, 300],
+  "address": {
+    "street": "Main Street",
+    "city": "Springfield"
+  }
+}
+''';
+
+  final ASTNodeWithLocation ast = JsonAstDecoder.withLocation(
+    allowInputAfter: true,
+  ).convert(json);
+
+  print(ast);
+}
